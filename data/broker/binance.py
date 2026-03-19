8f95326ee5323c2370061a1d1f804a75e620fc01
osonseth <max.cesaria@gmail.com>
2026-03-19 04:39:49 +0100

    wip: BinanceClient fetch_candles, populate_history, error handling

--- data/broker/binance.py
+++ data/broker/binance.py
@@ -1,29 +1,36 @@
 import ccxt
-import pandas as pd
 from base import BrokerBase
+import time
 
 class BinanceClient(BrokerBase):
-    CANDLE_COLUMNS = ["timestamp", "open", "highest", "lowest", "closing", "vol"]
 
     def __init__(self):
         super().__init__('https://api.binance.com')
-        self.exchange = ccxt.binance()
-    
-    def fetch_candles(self, asset: str, timeframe: str, since: int = None):
-        all_candles = []
+        self.exchange = ccxt.binance({'enableRateLimit': True})
+        self.start_timestamp = self.exchange.parse8601('2017-08-16T00:00:00Z')
+        self.default_limit = 1000
+
+    def fetch_candles(self, asset: str, timeframe: str, limit: int = None, since: int = None)-> list[list]:
+        max_retries = 3
         if since is None:
-            since = self.exchange.parse8601('2017-08-16T00:00:00Z')
-        while True:
-            response = self.exchange.fetch_ohlcv(asset,timeframe, since = since,)
-            all_candles.extend(response)
-            if len(response) < 1000:
+            since = self.start_timestamp
+        if limit is None:
+            limit = self.default_limit
+        for attempt in range(max_retries):
+            try:
+                response = self.exchange.fetch_ohlcv(asset, timeframe, since = since, limit = limit)
                 break
-            if timeframe == '1M':
-                since = pd.Timestamp(all_candles[-1][0], unit='ms') + pd.DateOffset(months=1)
-                since = int(since.timestamp() * 1000)
-            else:
-                since = all_candles[-1][0] + self.exchange.parse_timeframe(timeframe) * 1000
-        df = pd.DataFrame(all_candles, columns=self.CANDLE_COLUMNS)
-        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-        print(df)
-        df.to_csv('test_candles.csv', index=False)
+            except ccxt.ExchangeError:
+                # logger
+                raise
+            except (ccxt.NetworkError, ccxt.RateLimitExceeded):
+                if attempt == max_retries - 1:
+                    # logger et stopper
+                    raise
+                wait = 2 ** attempt
+                time.sleep(wait)
+            except Exception:
+                #logger
+                raise
+        return response
+    
