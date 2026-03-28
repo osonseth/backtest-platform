757be01848c6a84b14a0c337fcc77dc323ce95e3
osonseth <max.cesaria@gmail.com>
2026-03-28 21:38:42 +0100

    feat: add WebSocket stream support to BinanceClient

--- data/broker/binance.py
+++ data/broker/binance.py
@@ -1,5 +1,7 @@
 import ccxt
 import time
+import websockets
+from typing import AsyncGenerator
 from data.broker.base import BrokerBase
 from utils.logger import get_logger
 
@@ -13,6 +15,9 @@ class BinanceClient(BrokerBase):
         self.exchange = ccxt.binance({'enableRateLimit': True})
         self.start_timestamp = self.exchange.parse8601('2017-01-01T00:00:00Z')
         self.default_limit = 1000
+        self.ws_base_url = "wss://stream.binance.com:9443/stream?streams="
+        self.ws_stream_url = ""
+        self.ws_symbol_map = {}
 
     def fetch_candles(self, asset: str, timeframe: str, limit: int = None, since: int = None)-> list[list]:
         """
@@ -46,4 +51,36 @@ class BinanceClient(BrokerBase):
             except Exception as e:
                 raise
         return response
+    
+    def _build_assets_symbols_streams_map (self, assets: list)-> None:
+        """
+        Build a mapping from WebSocket symbol format to standard asset format.
+        Stores the result in self.ws_symbol_map.
+        :param assets: List of asset names in standard format (e.g. ['BTC/USDT', 'ETH/USDT'])
+        """
+        self.ws_symbol_map = {asset.replace('/', '').lower(): asset for asset in assets}
+
+    def _build_stream_url(self, assets: list)-> None:
+        """
+        Build the WebSocket combined stream URL for all given assets.
+        Stores the result in self.ws_stream_url.
+        :param assets: List of asset names in standard format (e.g. ['BTC/USDT', 'ETH/USDT'])
+        """
+        streams = [asset.replace('/', '').lower() + "@kline_1m" for asset in assets]
+        self.ws_stream_url = self.ws_base_url + '/'.join(streams)
+
+    async def connect_stream(self, assets: list)-> AsyncGenerator[str, None]:
+        """
+        Open a WebSocket connection to Binance and yield raw messages as they arrive.
+        Subscribes to 1m kline streams for all given assets via a combined stream.
+        :param assets: List of asset names in standard format (e.g. ['BTC/USDT', 'ETH/USDT'])
+        :return: Async generator yielding raw JSON messages as strings
+        """
+        self._build_assets_symbols_streams_map(assets)
+        self._build_stream_url(assets)
+        async with websockets.connect(self.ws_stream_url) as ws:
+            while True:
+                msg = await ws.recv()
+                yield msg
+
     
