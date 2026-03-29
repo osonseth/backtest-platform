2b3c2fd01b9d83ffc72256bcb02ed780d7e7157e
osonseth <max.cesaria@gmail.com>
2026-03-29 01:15:56 +0100

    feat: add backfill in candle_stream

--- data/fetch/candles_stream.py
+++ data/fetch/candles_stream.py
@@ -1,13 +1,31 @@
 import asyncio
 import json
+from datetime import datetime, timedelta
 from data.broker.binance import BinanceClient
 from data.repository.db import Database
+from data.validate.candle_validator import CandleValidator
 from utils.candle_utils import normalizes_candle
 from utils.candle_utils import format_candles_for_db
 from utils.logger import get_logger
 
 logger = get_logger(__name__)
 
+
+def backfill(asset_ids: dict, timeframe_id:int, database: Database, exchange: BinanceClient)-> None:
+   
+   for asset, asset_id in asset_ids.items():
+        validator = CandleValidator()
+        last_timestamp = database.get_last_candle_timestamp(asset_id, timeframe_id)
+        since = int(last_timestamp.timestamp() * 1000) + 1 
+        while True:
+            candles = exchange.fetch_candles(asset, "1m", since = since)
+            if candles:
+                validator.validate(candles, "1m")
+                since =  candles[-1][0] + 1
+                candles = format_candles_for_db(candles, asset_id, timeframe_id)
+                database.insert_candles(candles)
+            if len(candles) < exchange.default_limit:
+                break
 
 async def main():
     path = "config/initial_streams.json"
@@ -19,7 +37,7 @@ async def main():
 
         timeframe_id = database.get_timeframe_id("1m")
         asset_ids = {asset: database.get_asset_id(asset) for asset in data["assets"]}
-
+        backfill(asset_ids, timeframe_id, database, exchange)
         async for msg in exchange.connect_stream(data["assets"]):
             data = json.loads(msg)
             candle = data["data"]["k"]
@@ -31,6 +49,7 @@ async def main():
                 candle = normalizes_candle(candle)
                 candle = format_candles_for_db([candle], asset_id, timeframe_id)
                 database.insert_candle_stream(candle)
+                
     except Exception as e:
         logger.error(f"{e}")
 
