0039f74bcff5bf1192497561ffa44fd74a0ce6dc
osonseth <max.cesaria@gmail.com>
2026-03-29 19:35:35 +0200

    feat: add last_seen_ts tracking to backfill

--- data/fetch/candles_stream.py
+++ data/fetch/candles_stream.py
@@ -10,20 +10,28 @@ from utils.logger import get_logger
 
 logger = get_logger(__name__)
 
+def backfill(asset_ids: dict, timeframe_id: int, last_seen_ts: dict, database: Database, exchange: BinanceClient) -> None:
 
-def backfill(asset_ids: dict, timeframe_id:int, database: Database, exchange: BinanceClient)-> None:
-   
-   for asset, asset_id in asset_ids.items():
+    for asset, asset_id in asset_ids.items():
         validator = CandleValidator()
-        last_timestamp = database.get_last_candle_timestamp(asset_id, timeframe_id)
-        since = int(last_timestamp.timestamp() * 1000) + 1 
+        
+        db_last_ts = database.get_last_candle_timestamp(asset_id, timeframe_id)
+        fetch_since_ms = int(db_last_ts.timestamp() * 1000) + 1
+
         while True:
-            candles = exchange.fetch_candles(asset, "1m", since = since)
+            candles = exchange.fetch_candles(asset, "1m", since=fetch_since_ms)
+
             if candles:
                 validator.validate(candles, "1m")
-                since =  candles[-1][0] + 1
+
+                last_candle_ts = candles[-1][0]
+                fetch_since_ms = last_candle_ts + 1
+
+                last_seen_ts[asset_id] = last_candle_ts
+
                 candles = format_candles_for_db(candles, asset_id, timeframe_id)
                 database.insert_candles(candles)
+
             if len(candles) < exchange.default_limit:
                 break
 
@@ -34,10 +42,10 @@ async def main():
         database = Database()
         with open(path) as f:
             data = json.load(f)
-
+        last_timestamps = {}  
         timeframe_id = database.get_timeframe_id("1m")
         asset_ids = {asset: database.get_asset_id(asset) for asset in data["assets"]}
-        backfill(asset_ids, timeframe_id, database, exchange)
+        backfill(asset_ids, timeframe_id,last_timestamps, database, exchange)
         async for msg in exchange.connect_stream(data["assets"]):
             data = json.loads(msg)
             candle = data["data"]["k"]
